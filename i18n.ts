76a6579a17117fb8ab4ee2bf985469a6842964ae
Reiraku <mateo.orodaz4@mail.com>
2026-05-08 20:46:37 -0300

    Se modifico el i18n por el momento para que este vacio

--- i18n.ts
+++
@@ -1,7 +0,0 @@
-import 'intl-pluralrules'
-import i18n from 'i18next'
-import { initReactI18next } from 'react-i18next'
-import es from '../locales/es.json'
-import en from '../locales/en.json'
-import ja from '../locales/ja.json'
-import ko from '../locales/ko.json'
